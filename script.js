b65bfb147551f5836308076c5fa3e4347feac967
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-21 11:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+//your JS code here. If required.<!DOCTYPE html>
+
+    function showAlert(){
+    let firsname = document.getElementById("name").value;
+    let lastname = document.getElementById("lastname").value;
+    let phone = document.getElementById("phoneNumber").value;
+    let email = document.getElementById("email"); 
+    if(!firsname || !lastname || !phone || !email){
+        alert("all fields are required")
+        return ;
+    } 
+    let message = `First Name:${firsname} Last Name : ${lastname} Phone Number: ${phone} Email Id:${email}`;
+    alert(message);
+    }
+
+  
+    
+
